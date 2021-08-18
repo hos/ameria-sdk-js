4245c9040517ca6d5d32646e0b3823dbcf3d0383
hos <hovosanoyan@gmail.com>
2021-08-18 19:48:27 +0400

    Update tsconfig.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,14 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["prettier"],
   extends: ["prettier", "plugin:prettier/recommended"],
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
   rules: {
     "prettier/prettier": "error",
   },
